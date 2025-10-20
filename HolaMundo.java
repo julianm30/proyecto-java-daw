6d8e72f4946aba40b01278fc9c24eae065ebea3c
julianm30 <jmonesp768@g.educaand.es>
2025-10-20 10:02:22 +0200

    eso

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
-    }
+    System.out.println(" mecagonto");}
 }

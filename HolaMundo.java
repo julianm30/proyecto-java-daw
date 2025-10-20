7a961fc10d0f2f26104c5148277042dc25aea701
julianm30 <jmonesp768@g.educaand.es>
2025-10-20 09:49:11 +0200

    Cambio rama nueva

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
+        
+        System.out.println("no se q es esto");
+        System.out.println("yo no se esto que coño hace pero weno");
+    }
+}
